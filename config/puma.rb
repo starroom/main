df21d3857769f243d6866dc4a65dac0ef60c894a
littlell <littlell@126.com>
2014-08-13 00:11:16 +0800

    111

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,12 @@
 #!/usr/bin/env puma
 
 environment "production"
-basedir = "/Users/littlell/work/starroom"
+basedir = "/home/developer/starroom/current"
 daemonize true
 threads 2,16
 
-# bind  "unix:///path/www/project_name/shared/tmp/sockets/puma.sock"
-bind "unix:///tmp/puma.sock"
-# pidfile  "#{basedir}/current/tmp/puma/pid"
-pidfile "/tmp/puma.pid"
-# state_path "#{basedir}/current/tmp/puma/state"
-state_path "/tmp/puma.state"
+#bind  "unix:///home/developer/starroom/tmp/puma.sock"
+#pidfile  "#{basedir}/current/tmp/puma.pid"
+state_path "#{basedir}/current/tmp/puma/state"
 preload_app!
 activate_control_app
