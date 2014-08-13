78289a10f43ad15e16ce3b0b25b4797ab45371fa
littlell <littlell@126.com>
2014-08-13 16:26:02 +0800

    111

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :check_token, except: :index
 
   def index
-    render :text => 'index!555'
+    render :text => 'index!666'
   end
 
   def mobile_new
